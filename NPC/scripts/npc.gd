aa414ab28ed2c8ff04459490fb2b9eb5abb35e7a
CptTiki <CptTiki@gmail.com>
2024-08-15 15:40:15 -0400

    NPC Behavior
    wander behavior
    constrain to node

--- NPC/scripts/npc.gd
+++ NPC/scripts/npc.gd
@@ -7,8 +7,9 @@ signal do_behavior_enabled
 var state: String = "idle"
 var direction : Vector2 = Vector2.DOWN
 var direction_name : String = "down"
+var do_behavior : bool = true
 
-@export var npc_resource : NPCResource
+@export var npc_resource : NPCResource : set = _set_npc_resource
 
 @onready var sprite = $Sprite2D
 @onready var animation_player = $AnimationPlayer
@@ -18,8 +19,41 @@ func _ready() -> void:
 	setup_npc()
 	if Engine.is_editor_hint():
 		return 
+	do_behavior_enabled.emit()
 	pass
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+
+
+func update_animation() -> void:
+	animation_player.play(state + "_" + direction_name)
+
+func update_direction(target_position : Vector2) -> void:
+	direction = global_position.direction_to(target_position)
+	update_direction_name()
+	if direction_name == "side" and direction.x < 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+
+func update_direction_name() -> void:
+	var threshold : float = 0.45
+	if direction.y < -threshold:
+		direction_name = "up"
+	elif direction.y > threshold:
+		direction_name = "down"
+	elif direction.x > threshold || direction.x < -threshold:
+		direction_name = "side"
+
 
 func setup_npc() -> void:
 	if npc_resource:
-		sprite.texture = npc_resource.sprite
+		if sprite:
+			sprite.texture = npc_resource.sprite
+
+
+func _set_npc_resource(_npc : NPCResource) -> void:
+	npc_resource = _npc
+	setup_npc()
+	pass
